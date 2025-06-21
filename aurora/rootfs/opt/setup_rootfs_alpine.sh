c8adb6938ea79602bb27229dec853438f37745b1
Sophie <sophiaasophieee@gmail.com>
2025-06-21 01:31:32 -0400

    lmfao

--- aurora/rootfs/opt/setup_rootfs_alpine.sh
+++ aurora/rootfs/opt/setup_rootfs_alpine.sh
@@ -4,10 +4,4 @@ set -e
 arch="${1}"
 
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-apk add wpa_supplicant vboot-utils curl tpm2-tools cryptsetup
-apk del apk-tools
-rm -rf /lib/rc
-rm -rf /var/cache
-rm /sbin/eapol_test
-rm /usr/lib/libstdc++* /usr/lib/libunistring*
-rm -rf /usr/share/vboot\
+apk add wpa_supplicant vboot-utils curl tpm2-tools cryptsetup coreutils bash
