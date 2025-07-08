e06764f939a5129fa02f3f3f2756fc931a2f3735
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-08 06:04:03 -0400

    :(

--- rootfs/usr/share/aurora/resize.sh
+++ rootfs/usr/share/aurora/resize.sh
@@ -10,5 +10,6 @@ dev_partition=$(blkid | grep 'LABEL="Aurora"' | awk -F: '{print $1}')
 dev=$(echo "$dev_partition" | sed -E 's/p?[0-9]+$//')
 
 parted -fs $dev align-check opt 4
+parted $dev resizepart 4 100%
 resize2fs "$dev_partition"
 rm -f /.UNRESIZED
