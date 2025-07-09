e17d9d86e25dd2c498580b473276692325d70d99
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-08 20:46:45 -0400

    update

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -744,6 +744,14 @@ downloadreco() {
 	unzip $VERSION.zip
 	rm $VERSION.zip
 }
+updateshim() {
+    apk add github-cli git
+    gh auth login
+    git clone https://github.com/EtherealWorkshop/Aurora /root/Aurora
+    cp -Lar /root/Aurora/rootfs/. /
+    cp -Lar /root/Aurora/$(uname -m)/. /
+    sync
+}
 
 downloadshim() {
 	cd $aroot/images/shims
