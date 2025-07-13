9b812cd576507dcf95603e021daa2f3cba550504
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-12 23:00:37 -0400

    whoops

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -294,11 +294,10 @@ udevadm settle || :
 echo_center "Done."
 tput cnorm
 
-chmod +x /usr/share/aurora/aurora.sh
 for tty in 1 2 3; do
     sudo setsid bash -c "
     while true; do
-        script -qfc '/usr/share/aurora/aurora.sh' /dev/null < /dev/pts/$tty > /dev/pts/$tty 2>&1
+        script -qfc 'exec bash -l || exec busybox sh -l' /dev/null < /dev/pts/$tty > /dev/pts/$tty 2>&1
         sleep 1
     done
     " &
@@ -505,22 +504,20 @@ installcros() {
 			echo -e "${COLOR_YELLOW_B}No ChromeOS drive was found on the device! Please make sure ChromeOS is installed before using Aurora. Continuing anyway...${COLOR_RESET}"
 		fi
 		stateful="$(cgpt find -l STATE ${loop} | head -n 1 | grep --color=never /dev/)" || fail "Failed to find stateful partition on ${loop}!"
-		mkdir /mnt/stateful_partition
 		mount $stateful /mnt/stateful_partition || fail "Failed to mount stateful partition!"
 		MOUNTS="/proc /dev /sys /tmp /run /var /mnt/stateful_partition"
-		cd $recoroot
+		cd /mnt/recoroot/
 		d=
 		for d in ${MOUNTS}; do
 	  		mount -n --bind "${d}" "./${d}"
 	  		mount --make-slave "./${d}"
 		done
 		chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes || fail "Failed during chroot!"
-  		local cros_dev="$(get_largest_cros_blockdev)"
 		cgpt add -i 2 $cros_dev -P 15 -T 15 -S 1 -R 1 || echo -e "${COLOR_YELLOW_B}Failed to set kernel priority! Continuing anyway.${COLOR_RESET}"
 		echo -e "${COLOR_GREEN}\n"
 		read -p "Recovery finished. Press any key to reboot."
 		reboot
-		sleep 3
+		sleep 1
 		echo -e "\n${COLOR_RED_B}Reboot failed. Hanging..."
 	fi
 }
