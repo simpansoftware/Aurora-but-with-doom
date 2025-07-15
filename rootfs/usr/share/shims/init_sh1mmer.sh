60a164a63ccfc29c2e6cd8d0f627f550d180d920
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-15 18:54:53 -0400

    e

--- rootfs/usr/share/shims/init_sh1mmer.sh
+++ rootfs/usr/share/shims/init_sh1mmer.sh
@@ -196,6 +196,7 @@ mkdir -p $NEWROOT_MNT/tmp/oldroot
 pivot_root $NEWROOT_MNT $NEWROOT_MNT/tmp/oldroot
 mkdir -p /tmp/aurora
 mkdir -p /tmp/shimroot
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin"
 mount -n -o move $(findmnt /tmp/oldroot/tmp/aurora -o SOURCE | tail -n1) /tmp/aurora || echo "Failed to move mountpoint" && sleep 1
 mount -n -o move $(findmnt /tmp/oldroot/tmp/aurora/shimroot -o SOURCE | tail -n1) /tmp/shimroot || echo "Failed to move mountpoint" && sleep 1
 exec /usr/sbin/sh1mmer_main.sh
