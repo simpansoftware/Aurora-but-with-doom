08ed27ac75f4c914e6f372df976f640a0655e6f6
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-22 08:43:16 -0400

    rootfsupdatepackages.sh

---
+++ rootfs/opt/rootfsupdatepackages.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apk add jq rsync cgpt file unzip dhcpcd fastfetch figlet util-linux syslog-ng e2fsprogs fish lsblk losetup wpa_supplicant vboot-utils curl tpm2-tools cryptsetup coreutils bash openrc dbus eudev udev-init-scripts ncurses udisks2 sudo zram-init iw wpa_supplicant cloud-utils-growpart nano sfdisk sgdisk
