41563d49b9fbe1fb800635db4b8eaec555ecefb5
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-23 07:48:53 -0400

    :(

---
+++ initramfs/switchroot.sh
@@ -0,0 +1,28 @@
+#!/bin/busybox sh
+echo "Making the newroot partition..."
+mkdir -p /newroot
+dev_partition=$(blkid | grep 'LABEL="Aurora"' | awk -F: '{print $1}' || true)
+dev=$(echo "$dev_partition" | sed -E 's/p?[0-9]+$//')
+if [ -z "$dev_partition" ]; then
+    echo "failed to find aurora partition"
+    exit 1
+fi
+if [ -f "/.UNRESIZED" ]; then
+    chmod +x /.UNRESIZED
+    bash /.UNRESIZED
+    rm -f /.UNRESIZED
+    sync
+fi
+echo "Mounting device..."
+mount $dev_partition /newroot || mount /dev/sda4 /newroot || mount /dev/mmcblk1p4 /newroot # incase of thuggery from dev_partition
+for mnt in /dev /proc /sys; do
+    mkdir -p "/newroot$mnt"
+    mount -n -o move "$mnt" "/newroot$mnt"
+done
+
+echo "pivoting root"
+mkdir -p /newroot/initramfs
+pivot_root /newroot /newroot/initramfs
+chmod +x /newroot/sbin/init
+echo "exec /sbin/init"
+exec /sbin/init < "$TTY1" >> "$TTY1" 2>&1
