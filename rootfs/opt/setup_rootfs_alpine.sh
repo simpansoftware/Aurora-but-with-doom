8e29ec5464c3e88579d438e890cae2edf7dfeb18
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-24 23:48:43 -0400

    e

--- rootfs/opt/setup_rootfs_alpine.sh
+++ rootfs/opt/setup_rootfs_alpine.sh
@@ -7,20 +7,20 @@ http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 apk add jq debootstrap rsync cgpt file unzip dhcpcd tzdata fastfetch figlet util-linux syslog-ng e2fsprogs fish lsblk losetup wpa_supplicant vboot-utils curl tpm2-tools cryptsetup coreutils bash openrc dbus eudev udev-init-scripts ncurses udisks2 sudo zram-init iw wpa_supplicant cloud-utils-growpart nano sfdisk sgdisk wget gnupg tar xz zstd
 cat <<'EOF' >> /etc/profile
+if [ "$USER" = "root" ]; then
+  PS1='\[\e[1;31m\]\u@\h \[\e[1;33m\]$(date +"%H:%M %b %d")\[\e[1;32m\] \w/\[\e[0m\] '
+else
+  PS1='\[\e[1;34m\]\u@\h \[\e[1;33m\]$(date +"%H:%M %b %d")\[\e[1;32m\] \w/\[\e[0m\] '
+fi
+
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias grep='grep --color=auto'
+alias reboot='reboot -f'
+alias toilet="figlet" # those who know
+
+echo ""
 if [ -z "$login" ]; then
-  if [ "$USER" = "root" ]; then
-    PS1='\[\e[1;34m\]\u@\h \[\e[1;33m\]$(date +"%H:%M %b %d")\[\e[1;32m\] \w/\[\e[0m\] '
-  else
-    PS1='\[\e[1;31m\]\u@\h \[\e[1;33m\]$(date +"%H:%M %b %d")\[\e[1;32m\] \w/\[\e[0m\] '
-  fi
-
-  alias ls='ls --color=auto'
-  alias dir='dir --color=auto'
-  alias grep='grep --color=auto'
-  alias reboot='reboot -f'
-  alias toilet="figlet" # those who know
-
-  echo ""
   cat /etc/motd
   read -p "$(hostname) login: " USER
   export login=1
