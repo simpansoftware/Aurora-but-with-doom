57070fc3b74de4044ca01ebe438b661f44cf354f
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-08-12 19:26:21 -0400

    a

--- rootfs/opt/setup_rootfs_alpine.sh
+++ rootfs/opt/setup_rootfs_alpine.sh
@@ -1,11 +1,45 @@
 #!/bin/sh
 
+setup-hostname Aurora
+
 cat <<EOF > /etc/apk/repositories
 http://dl-cdn.alpinelinux.org/alpine/edge/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 apk add jq debootstrap rsync cgpt file unzip dhcpcd tzdata fastfetch figlet util-linux syslog-ng e2fsprogs fish lsblk losetup wpa_supplicant vboot-utils curl tpm2-tools cryptsetup coreutils bash openrc dbus eudev udev-init-scripts ncurses udisks2 sudo zram-init iw wpa_supplicant cloud-utils-growpart nano sfdisk sgdisk wget gnupg tar xz zstd >/dev/null
+
+rc-update add acpid default
+rc-update add bootmisc boot
+rc-update add crond default
+rc-update add devfs sysinit
+rc-update add sysfs sysinit
+rc-update add dmesg sysinit
+rc-update add hostname boot
+rc-update add hwclock boot
+rc-update add hwdrivers sysinit
+rc-update add killprocs shutdown
+rc-update add mdev sysinit
+rc-update add modules boot
+rc-update add mount-ro shutdown
+rc-update add networking boot
+rc-update add savecache shutdown
+rc-update add seedrng boot
+rc-update add swap boot
+rc-update add syslog boot
+chmod +x /etc/init.d/aurora
+rc-update add aurora boot
+
+rc-update add iwd default
+rc-update add wpa_supplicant default
+rc-update add zram-init default
+rc-update add dbus default
+
+sed -i 's/=zstd/=lzo/' /etc/conf.d/zram-init
+sed -i '/size0=512/d' /etc/conf.d/zram-init
+sed -i '/blk1=1024/d' /etc/conf.d/zram-init
+echo "size0=\`LC_ALL=C free -m | awk '/^Mem:/{print int(\$2/2)}'\`" >> /etc/conf.d/zram-init
+
 cat <<'EOF' >> /etc/profile
 if [ "$USER" = "root" ]; then
   PS1='\e[1;31m\u@\h \e[1;33m$(date +"%H:%M %b %d")\e[1;32m \w/\e[0m '
@@ -38,8 +72,6 @@ for module in $modules; do
   cat "/etc/modules-load.d/$module" >> /etc/modules
   echo >> /etc/modules
 done
-echo "Aurora" > /etc/hostname
-echo "127.0.0.1 localhost Aurora" >> /etc/hosts
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo root:root | chpasswd 2>/dev/null
 rm -f /etc/setup /etc/shimboot
