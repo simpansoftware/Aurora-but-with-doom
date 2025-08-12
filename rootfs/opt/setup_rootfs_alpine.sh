5a115734ae5ba8e0f17eca96f1d5f18df4714bd5
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-08-12 19:27:08 -0400

    iwd

--- rootfs/opt/setup_rootfs_alpine.sh
+++ rootfs/opt/setup_rootfs_alpine.sh
@@ -7,7 +7,7 @@ http://dl-cdn.alpinelinux.org/alpine/edge/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
-apk add jq debootstrap rsync cgpt file unzip dhcpcd tzdata fastfetch figlet util-linux syslog-ng e2fsprogs fish lsblk losetup wpa_supplicant vboot-utils curl tpm2-tools cryptsetup coreutils bash openrc dbus eudev udev-init-scripts ncurses udisks2 sudo zram-init iw wpa_supplicant cloud-utils-growpart nano sfdisk sgdisk wget gnupg tar xz zstd >/dev/null
+apk add jq debootstrap rsync iwd cgpt file unzip dhcpcd tzdata fastfetch figlet util-linux syslog-ng e2fsprogs fish lsblk losetup wpa_supplicant vboot-utils curl tpm2-tools cryptsetup coreutils bash openrc dbus eudev udev-init-scripts ncurses udisks2 sudo zram-init iw wpa_supplicant cloud-utils-growpart nano sfdisk sgdisk wget gnupg tar xz zstd >/dev/null
 
 rc-update add acpid default
 rc-update add bootmisc boot
