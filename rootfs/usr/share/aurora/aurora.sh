ffd8f1cc07f71b670e1c09a9434126f6a8c36698
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-08-15 01:15:40 -0400

    unneeded i think

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -512,35 +512,6 @@ installcros() {
     fail "Reboot failed." --fatal
 }
 
-is_ext2() {
-    local rootfs="$1"
-    local offset="${2-0}"
-
-    local sb_magic_offset=$((0x438))
-    local sb_value=$(dd if="$rootfs" skip=$((offset + sb_magic_offset)) \
-        count=2 bs=1 2>/dev/null)
-    local expected_sb_value=$(printf '\123\357')
-    if [ "$sb_value" = "$expected_sb_value" ]; then
-        return 0
-    fi
-    return 1
-}
-
-enable_rw_mount() {
-    local rootfs="$1"
-    local offset="${2-0}"
-
-    if ! is_ext2 "$rootfs" $offset; then
-        echo "enable_rw_mount called on non-ext2 filesystem: $rootfs $offset" 1>&2
-        return 1
-    fi
-
-    local ro_compat_offset=$((0x464 + 3))
-    printf '\000' |
-        dd of="$rootfs" seek=$((offset + ro_compat_offset)) \
-            conv=notrunc count=1 bs=1 2>/dev/null
-}
-
 shimboot() {
     chmod +x /usr/bin/bigtext
     bigtext shimboot
@@ -589,7 +560,6 @@ shimboot() {
         loop_root="${loop}p3"
     fi
 
-    enable_rw_mount ${loop_root}
     if mount "${loop_root}" $shimroot; then
         echo -e "ROOT-A found successfully and mounted." | center
     else
