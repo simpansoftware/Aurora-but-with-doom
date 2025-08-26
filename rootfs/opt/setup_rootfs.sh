e37e9f7610ed302d171df3d9c6c7e90602d3f18f
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-08-25 22:32:57 -0400

    nugget

--- rootfs/opt/setup_rootfs.sh
+++ rootfs/opt/setup_rootfs.sh
@@ -16,3 +16,4 @@ echo "Aurora" > /etc/hostname
 echo "127.0.0.1 localhost Aurora" >> /etc/hosts
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo root:root | chpasswd 2>/dev/null
+sed -i 's/setup=0/setup=1/' /etc/aurora
