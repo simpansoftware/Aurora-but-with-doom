e671a8fcc019113c18daa15b6f62bdaf7802b3dc
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-01 16:30:18 -0400

    Update aurora.sh

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -1154,7 +1154,7 @@ EOF
     udevadm trigger | center || :
     udevadm settle | center || :
 fi
-clear
+printf '\033c' > /dev/pts/2
 for wifi in iwlwifi iwlmvm ccm 8021q rtw88 rtwpci ath10k_sdio mt7921e mt7921s mt76; do
     modprobe -r "$wifi" 2>$LOGTTY || true
     modprobe "$wifi" 2>$LOGTTY
@@ -1213,11 +1213,11 @@ while true; do
     tput civis
     stty -echo
     menu "Select an option (use ← → ↑ ↓ arrows, Enter to select)" -p "${current_options[@]}"
+    choice=$?
     if [[ -n "$updatedpage" && "$updatedpage" == "1" ]]; then
         updatedpage=0
         continue
     fi
-    choice=$?
     action="${current_actions[$choice]}"
     option="${current_options[$choice]}"
     echo ""
