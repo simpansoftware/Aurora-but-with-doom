5dc166a33a26dfdbcdf9a20f84769676ff6bc8b9
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-01 22:15:33 -0400

    uh

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -1161,10 +1161,11 @@ fi
 printf '\033c' > $LOGTTY
 
 
-for wifi in iwlwifi iwlmvm ccm 8021q rtw88 rtwpci ath10k_sdio mt7921e mt7921s mt76; do
+for wifi in iwlwifi iwlmvm ccm 8021q rtw88 rtwpci ath10k_sdio mt7921e mt7921s mt76 rtw88_8822ce rtw8821ce rtw89pci; do
     modprobe -r "$wifi" 2>$LOGTTY || true
     modprobe "$wifi" 2>$LOGTTY
 done
+modprobe -a iwlwifi iwlmvm ccm 8021q rtw88 rtwpci ath10k_sdio mt7921e mt7921s mt76 rtw88_8822ce rtw8821ce rtw89pci 2>/dev/null
 sleep 2
 if [ -e "/etc/wpa_supplicant.conf" ]; then
     ls /etc/wpa_supplicant.conf >$LOGTTY 2>&1
